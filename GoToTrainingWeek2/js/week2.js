d391732c8a8655924acd603a19c28e53f42b798c
rbodell <bodellrebecca@gmail.com>
2015-09-10 20:06:25 -0400

    prompted user for name
    prompted user for name and alerted hello

--- GoToTrainingWeek2/js/week2.js
+++ GoToTrainingWeek2/js/week2.js
@@ -6,3 +6,12 @@ GoTo Training Week #2
  */
 
 //alert("Test to see if connected");
+
+// Create an age Calculator
+
+//Ask the user for their name
+var name = prompt("Let's figure out how old you are!\nPlease type in your name.");
+
+//User the variable and say hello
+alert("Welcome "+name+"!");
+
