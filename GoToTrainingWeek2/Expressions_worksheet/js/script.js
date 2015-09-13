554482c0190da3aa278c9fd477efd3e9af937474
rbodell <bodellrebecca@gmail.com>
2015-09-13 01:14:30 -0400

    Random

--- GoToTrainingWeek2/Expressions_worksheet/js/script.js
+++ GoToTrainingWeek2/Expressions_worksheet/js/script.js
@@ -4,11 +4,3 @@
  */
 
 
-// Note what sparky's age is in human years
-var sparkysAge = 1;
-
-// Note that 1 human years age is 7 dog years
-var dogYears = sparkysAge * 7;
-
-// Multiple x amount of human years * 7 which results in Sparky's age
-console.log("Sparky is " + sparkysAge + " human years old which is " + dogYears + " in dog years.");
