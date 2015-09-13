9c34752f8e87e9a06be5a6a96cd80fbd7d6f0464
rbodell <bodellrebecca@gmail.com>
2015-09-13 01:41:13 -0400

    GoTo Training Conditionals
    Practicing

---
+++ GoToTrainingWeek3/js/script.js
@@ -0,0 +1,16 @@
+
+ /*
+Rebecca Chevalier
+SDI Section #1
+//Conditional Logic
+09-13-2015
+ */
+
+ var oldEnough = false;
+
+ //if the child is old enough, print to the console "you can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+ console.log("what comes after");
