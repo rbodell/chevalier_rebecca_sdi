1dfc0049af6b4b6f795b4e41ccce6a7a2522e44b
rbodell <bodellrebecca@gmail.com>
2015-09-12 22:22:14 -0400

    Continuing my calculator
    Adding in different variables

--- GoToTrainingWeek2/Chevalier_Rebecca_Expressions_Assignment/js/script.js
+++ GoToTrainingWeek2/Chevalier_Rebecca_Expressions_Assignment/js/script.js
@@ -15,8 +15,37 @@ Expressions Assignment
  //Fun comment to let the user know I retained the information
  alert("Oh, I love "+cablecompany+"!");
 
+ //console.log
+ console.log("Oh, I love "+cablecompany+"!");
+
  //Ask the user how much their cable bill is
  var cablebillcharge = prompt ("And how much is your bill with "+cablecompany+" each month?");
 
  //Funny comment to let the user know I retained more information
  alert("Wow... "+cablebillcharge+" is a lot of money just to watch Game of Thrones!");
+
+ //console.log
+ console.log("Wow... "+cablebillcharge+" is a lot of money just to watch Game of Thrones!");
+
+ //Ask the user how much money they make a month
+ var takehomepay = prompt ("I'm going to calculate how much money you'll have left over after your "+cablecompany+" bill now. How much is your monthly take home pay?");
+
+ //Funny comment to let the user know I retained the information
+ alert("Yikes. Only "+takehomepay+"? Hope you graduate Full Sail...")
+
+ //console.log
+ console.log("Yikes. Only "+takehomepay+"? Hope you graduate Full Sail...");
+
+ //Confirming begining of calculation
+ alert("Ok! Let's calculate!");
+
+ // Figure out how much you have left over after cable bill charge
+ var amountleftover = takehomepay - cablebillcharge;
+
+//Letting the user know the result
+ alert("So after your"+cablecompany+" bill of "+cablebillcharge+", you will have "+amountleftover+" to spend on other things!");
+
+//console.log
+ console.log("So after your "+cablecompany+" bill of "+cablebillcharge+", you will have "+amountleftover+" to spend on other things!");
+
+ 
