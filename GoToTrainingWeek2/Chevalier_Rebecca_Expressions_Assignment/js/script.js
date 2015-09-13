b45b2b25a17db951b45cc227b32bd43ccdcc4af9
rbodell <bodellrebecca@gmail.com>
2015-09-12 22:06:46 -0400

    Beginning of my Expressions Assignment
    Starting the calculator

---
+++ GoToTrainingWeek2/Chevalier_Rebecca_Expressions_Assignment/js/script.js
@@ -0,0 +1,22 @@
+
+ /*
+Rebecca Chevalier
+SDI Section #1
+Expressions Assignment
+09/10/2015
+ */
+
+// Letting the user know what to do
+ alert("Hi there! I am trying to figure out how much money you have left from your paycheck after you pay your cable bill! ");
+
+ //Asking the user what cable company they use
+ var cablecompany = prompt("What cable company do you use? ");
+
+ //Fun comment to let the user know I retained the information
+ alert("Oh, I love "+cablecompany+"!");
+
+ //Ask the user how much their cable bill is
+ var cablebillcharge = prompt ("And how much is your bill with "+cablecompany+" each month?");
+
+ //Funny comment to let the user know I retained more information
+ alert("Wow... "+cablebillcharge+" is a lot of money just to watch Game of Thrones!");
